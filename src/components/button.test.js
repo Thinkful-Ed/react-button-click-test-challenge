48473168069be8ee09dadd9ec83093f19595c460
Joe Turner <joe@oampo.co.uk>
2017-11-29 12:08:56 +0000

    Add solution

--- src/components/button.test.js
+++ src/components/button.test.js
@@ -5,7 +5,9 @@ import Button from './button';
 
 describe('<Button />', () => {
     it('Should call props.onClick when the button is clicked', () => {
-        const wrapper = shallow(<Button />);
+        const spy = jest.fn();
+        const wrapper = shallow(<Button onClick={spy} />);
         wrapper.find('button').simulate('click');
+        expect(spy).toHaveBeenCalled();
     });
 });
